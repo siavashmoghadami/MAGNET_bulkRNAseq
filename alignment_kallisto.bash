342fd2287658255ed7a1064895f50e66ae9d05c3
Siavash Richard Moghadami <51088825+siavashmoghadami@users.noreply.github.com>
2021-09-14 09:14:13 -0700

    Update alignment_kallisto.bash

--- alignment_kallisto.bash
+++ alignment_kallisto.bash
@@ -6,3 +6,6 @@ for i in {first accession number..last accession number}
 kallisto quant -i /path/to/index/Homo_sapiens.GRCh38.cdna.all.index -o /path/to/output/SRR$i -t number_of_threads --bias -b number_of_bootstraps \
 /path/to/trimmed_fastq1/trimmed_SRR$i"_1".fastq /path/to/trimmed_fastq2/trimmed_SRR$i"_2".fastq &> /path/to/log/SRR$i".log"
 done
+
+# Make a report with multiqc
+multiqc -d /path/to/project -o /path/to/output
