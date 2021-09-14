514f2197dd28be0812a553df8de94eae73862741
Siavash Richard Moghadami <51088825+siavashmoghadami@users.noreply.github.com>
2021-09-14 11:08:22 -0700

    Create MAGNET.R

---
+++ MAGNET.R
@@ -0,0 +1,11 @@
+# Introduction---- 
+# magnet project bulk RNA-seq analysis pipeline by Siavash Richard Moghadami
+# ----
+# loading required packages----
+# make sure to perform setRepositories()
+library(rhdf5)
+library(tidyverse)
+library(tximport)
+library(ensembldb)
+library(EnsDb.Hsapiens.v86)
+#----
